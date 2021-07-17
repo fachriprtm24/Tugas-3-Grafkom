1dca5328cc384d3e124bb23228ec2bbb98290d38
fachriprtm24 <57351353+fachriprtm24@users.noreply.github.com>
2021-07-17 21:06:34 +0700

    Update Demo.cpp

--- Demo.cpp
+++ Demo.cpp
@@ -16,8 +16,6 @@ void Demo::Init() {
 	// build and compile our shader program
 	// ------------------------------------
 	shaderProgram = BuildShader("vertexShader.vert", "fragmentShader.frag", nullptr);
-
-	//BuildColoredCube();
 	Kakikiridepanmeja();
 	Kakikiribelakangmeja();
 	Kakikanandepanmeja();
@@ -26,6 +24,8 @@ void Demo::Init() {
 	Cover();
 	Isi();
 	BuildColoredPlane();
+	InitCamera();
+
 }
 
 void Demo::DeInit() {
@@ -63,12 +63,80 @@ void Demo::DeInit() {
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
 void Demo::ProcessInput(GLFWwindow *window) {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, true);
+	}
+
+	// zoom camera
+	// -----------
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS) {
+		if (fovy < 90) {
+			fovy += 0.0001f;
+		}
+	}
+
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+		if (fovy > 0) {
+			fovy -= 0.0001f;
+		}
+	}
+
+	// update camera movement 
+	// -------------
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+		MoveCamera(CAMERA_SPEED);
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+		MoveCamera(-CAMERA_SPEED);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+		StrafeCamera(-CAMERA_SPEED);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+		StrafeCamera(CAMERA_SPEED);
+	}
+
+	// update camera rotation
+	// ----------------------
+	double mouseX, mouseY;
+	double midX = screenWidth / 2;
+	double midY = screenHeight / 2;
+	float angleY = 0.0f;
+	float angleZ = 0.0f;
+
+	// Get mouse position
+	glfwGetCursorPos(window, &mouseX, &mouseY);
+	if ((mouseX == midX) && (mouseY == midY)) {
+		return;
+	}
+
+	// Set mouse position
+	glfwSetCursorPos(window, midX, midY);
+
+	// Get the direction from the mouse cursor, set a resonable maneuvering speed
+	angleY = (float)((midX - mouseX)) / 1000;
+	angleZ = (float)((midY - mouseY)) / 1000;
+
+	// The higher the value is the faster the camera looks around.
+	viewCamY += angleZ * 2;
+
+	// limit the rotation around the x-axis
+	if ((viewCamY - posCamY) > 8) {
+		viewCamY = posCamY + 8;
+	}
+	if ((viewCamY - posCamY) < -8) {
+		viewCamY = posCamY - 8;
+	}
+	RotateCamera(-angleY);
+
+
+
 }
 
 void Demo::Update(double deltaTime) {
-
+	angle += (float)((deltaTime * 1.5f) / 1000);
 }
 
 void Demo::Render() {
@@ -89,9 +157,17 @@ void Demo::Render() {
 	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
 	// LookAt camera (position, target/direction, up)
-	glm::mat4 view = glm::lookAt(glm::vec3(3, 2, 5), glm::vec3(0, 0, 0), glm::vec3(0.0f, 1.0f, 0.0f));
+	glm::mat4 view = glm::lookAt(glm::vec3(posCamX, posCamY, posCamZ), glm::vec3(viewCamX, viewCamY, viewCamZ), glm::vec3(upCamX, upCamY, upCamZ));
 	GLint viewLoc = glGetUniformLocation(this->shaderProgram, "view");
 	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+	// set lighting attribute
+	GLint lightPosLoc = glGetUniformLocation(this->shaderProgram, "lightPos");
+	glUniform3f(lightPosLoc, posCamX, posCamY, posCamZ);
+	GLint viewPosLoc = glGetUniformLocation(this->shaderProgram, "viewPos");
+	glUniform3f(viewPosLoc, viewCamX, viewCamY, viewCamZ);
+	GLint lightColorLoc = glGetUniformLocation(this->shaderProgram, "lightColor");
+	glUniform3f(lightColorLoc, 1.0f, 1.0f, 1.0f);
 
 	//DrawColoredCube();
 	DrawKiridepan();
@@ -106,101 +182,7 @@ void Demo::Render() {
 	glDisable(GL_DEPTH_TEST);
 }
 
-//void Demo::BuildColoredCube() {
-//	// load image into texture memory
-//	// ------------------------------
-//	// Load and create a texture 
-//	glGenTextures(1, &texture);
-//	glBindTexture(GL_TEXTURE_2D, texture);
-//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-//	int width, height;
-//	unsigned char* image = SOIL_load_image("crate.png", &width, &height, 0, SOIL_LOAD_RGBA);
-//	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
-//	SOIL_free_image_data(image);
-//	glBindTexture(GL_TEXTURE_2D, 0);
-//
-//	// set up vertex data (and buffer(s)) and configure vertex attributes
-//	// ------------------------------------------------------------------
-//	float vertices[] = {
-//		// format position, tex coords
-//		// front
-//		-0.5, -0.5, 0.5, 0, 0,  // 0
-//		0.5, -0.5, 0.5, 1, 0,   // 1
-//		0.5,  0.5, 0.5, 1, 1,   // 2
-//		-0.5,  0.5, 0.5, 0, 1,  // 3
-//
-//		// right
-//		0.5,  0.5,  0.5, 0, 0,  // 4
-//		0.5,  0.5, -0.5, 1, 0,  // 5
-//		0.5, -0.5, -0.5, 1, 1,  // 6
-//		0.5, -0.5,  0.5, 0, 1,  // 7
-//
-//		// back
-//		-0.5, -0.5, -0.5, 0, 0, // 8 
-//		0.5,  -0.5, -0.5, 1, 0, // 9
-//		0.5,   0.5, -0.5, 1, 1, // 10
-//		-0.5,  0.5, -0.5, 0, 1, // 11
-//
-//		// left
-//		-0.5, -0.5, -0.5, 0, 0, // 12
-//		-0.5, -0.5,  0.5, 1, 0, // 13
-//		-0.5,  0.5,  0.5, 1, 1, // 14
-//		-0.5,  0.5, -0.5, 0, 1, // 15
-//
-//		// upper
-//		0.5, 0.5,  0.5, 0, 0,   // 16
-//		-0.5, 0.5,  0.5, 1, 0,  // 17
-//		-0.5, 0.5, -0.5, 1, 1,  // 18
-//		0.5, 0.5, -0.5, 0, 1,   // 19
-//
-//		// bottom
-//		-0.5, -0.5, -0.5, 0, 0, // 20
-//		0.5, -0.5, -0.5, 1, 0,  // 21
-//		0.5, -0.5,  0.5, 1, 1,  // 22
-//		-0.5, -0.5,  0.5, 0, 1, // 23
-//	};
-//
-//	unsigned int indices[] = {
-//		0,  1,  2,  0,  2,  3,   // front
-//		4,  5,  6,  4,  6,  7,   // right
-//		8,  9,  10, 8,  10, 11,  // back
-//		12, 14, 13, 12, 15, 14,  // left
-//		16, 18, 17, 16, 19, 18,  // upper
-//		20, 22, 21, 20, 23, 22   // bottom
-//	};
-//
-//	glGenVertexArrays(1, &VAO);
-//	glGenBuffers(1, &VBO);
-//	glGenBuffers(1, &EBO);
-//	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-//	glBindVertexArray(VAO);
-//
-//	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-//	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-//
-//	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-//	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-//
-//	// define position pointer layout 0
-//	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(0 * sizeof(GLfloat)));
-//	glEnableVertexAttribArray(0);
-//
-//	// define texcoord pointer layout 1
-//	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-//	glEnableVertexAttribArray(1);
-//
-//	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-//	glBindBuffer(GL_ARRAY_BUFFER, 0);
-//
-//	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-//	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-//	glBindVertexArray(0);
-//
-//	// remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
-//	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-//
-//}
+
 
 void Demo::Kakikiridepanmeja() {
 	// load image into texture memory
@@ -832,21 +814,6 @@ void Demo::Isi() {
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 }
-//void Demo::DrawColoredCube()
-//{
-//	glUseProgram(shaderProgram);
-//
-//	glActiveTexture(GL_TEXTURE0);
-//	glBindTexture(GL_TEXTURE_2D, texture);
-//	glUniform1i(glGetUniformLocation(this->shaderProgram, "ourTexture"), 0);
-//
-//	glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-//
-//	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-//
-//	glBindTexture(GL_TEXTURE_2D, 0);
-//	glBindVertexArray(0);
-//}
 
 void Demo::DrawKiridepan()
 {
@@ -857,6 +824,16 @@ void Demo::DrawKiridepan()
 	glUniform1i(glGetUniformLocation(this->shaderProgram, "ourTexture"), 0);
 
 	glBindVertexArray(VAO3); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+
+	glm::mat4 model;
+	model = glm::translate(model, glm::vec3(0, 0, 0));
+
+	model = glm::rotate(model, angle, glm::vec3(0, 1, 0));
+
+	model = glm::scale(model, glm::vec3(1, 1, 1));
+
+	GLint modelLoc = glGetUniformLocation(this->shaderProgram, "model");
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
 	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
@@ -874,6 +851,16 @@ void Demo::DrawKiribelakang()
 
 	glBindVertexArray(VAO4); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
 
+	glm::mat4 model;
+	model = glm::translate(model, glm::vec3(0, 0, 0));
+
+	model = glm::rotate(model, angle, glm::vec3(0, 1, 0));
+
+	model = glm::scale(model, glm::vec3(1, 1, 1));
+
+	GLint modelLoc = glGetUniformLocation(this->shaderProgram, "model");
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
 	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
 	glBindTexture(GL_TEXTURE_2D, 0);
@@ -889,6 +876,16 @@ void Demo::DrawKanandepan()
 	glUniform1i(glGetUniformLocation(this->shaderProgram, "ourTexture"), 0);
 
 	glBindVertexArray(VAO5); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+
+	glm::mat4 model;
+	model = glm::translate(model, glm::vec3(0, 0, 0));
+
+	model = glm::rotate(model, angle, glm::vec3(0, 1, 0));
+
+	model = glm::scale(model, glm::vec3(1, 1, 1));
+
+	GLint modelLoc = glGetUniformLocation(this->shaderProgram, "model");
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
 	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
@@ -906,6 +903,16 @@ void Demo::DrawKananbelakang()
 
 	glBindVertexArray(VAO6); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
 
+	glm::mat4 model;
+	model = glm::translate(model, glm::vec3(0, 0, 0));
+
+	model = glm::rotate(model, angle, glm::vec3(0, 1, 0));
+
+	model = glm::scale(model, glm::vec3(1, 1, 1));
+
+	GLint modelLoc = glGetUniformLocation(this->shaderProgram, "model");
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
 	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
 	glBindTexture(GL_TEXTURE_2D, 0);
@@ -922,6 +929,16 @@ void Demo::DrawBadan()
 
 	glBindVertexArray(VAO7); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
 
+	glm::mat4 model;
+	model = glm::translate(model, glm::vec3(0, 0, 0));
+
+	model = glm::rotate(model, angle, glm::vec3(0, 1, 0));
+
+	model = glm::scale(model, glm::vec3(1, 1, 1));
+
+	GLint modelLoc = glGetUniformLocation(this->shaderProgram, "model");
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
 	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
 	glBindTexture(GL_TEXTURE_2D, 0);
@@ -937,7 +954,7 @@ void Demo::DrawCover()
 	glUniform1i(glGetUniformLocation(this->shaderProgram, "ourTexture"), 0);
 
 	glBindVertexArray(VAO8); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-
+	
 	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
 	glBindTexture(GL_TEXTURE_2D, 0);
@@ -953,7 +970,7 @@ void Demo::DrawIsi()
 	glUniform1i(glGetUniformLocation(this->shaderProgram, "ourTexture"), 0);
 
 	glBindVertexArray(VAO9); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-
+	
 	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
 	glBindTexture(GL_TEXTURE_2D, 0);
@@ -962,29 +979,16 @@ void Demo::DrawIsi()
 
 void Demo::BuildColoredPlane()
 {
-	// Load and create a texture 
-	glGenTextures(1, &texture2);
-	glBindTexture(GL_TEXTURE_2D, texture2);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	int width, height;
-	unsigned char* image = SOIL_load_image("download.png", &width, &height, 0, SOIL_LOAD_RGBA);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
-	glGenerateMipmap(GL_TEXTURE_2D);
-	SOIL_free_image_data(image);
-	glBindTexture(GL_TEXTURE_2D, 0);
+	
 
 	// Build geometry
 	GLfloat vertices[] = {
-		// format position, tex coords
+		// format position, normals
 		// bottom
-		-50.0, -0.5, -50.0,  0,  0,
-		 50.0, -0.5, -50.0, 100,  0,
-		 50.0, -0.5,  50.0, 100, 100,
-		-50.0, -0.5,  50.0,  0, 100,
+		-50.0, -0.5, -50.0,  0.0f,  1.0f,  0.0f,
+		 50.0, -0.5, -50.0,  0.0f,  1.0f,  0.0f,
+		 50.0, -0.5,  50.0,  0.0f,  1.0f,  0.0f,
+		-50.0, -0.5,  50.0,  0.0f,  1.0f,  0.0f,
 
 
 	};
@@ -1004,34 +1008,84 @@ void Demo::BuildColoredPlane()
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// Position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), 0);
 	glEnableVertexAttribArray(0);
-	// TexCoord attribute
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	// Normal attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
 
 	glBindVertexArray(0); // Unbind VAO
 }
 
-
-
 void Demo::DrawColoredPlane()
 {
-	glUseProgram(shaderProgram);
-
-	glActiveTexture(GL_TEXTURE1);
-	glBindTexture(GL_TEXTURE_2D, texture2);
-	glUniform1i(glGetUniformLocation(this->shaderProgram, "ourTexture"), 1);
+	UseShader(shaderProgram);
 
 	glBindVertexArray(VAO2); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
 
-	glDrawElements(GL_TRIANGLES, 64, GL_UNSIGNED_INT, 0);
+	GLint objectColorLoc = glGetUniformLocation(this->shaderProgram, "objectColor");
+	glUniform3f(objectColorLoc, 0.8f, 0.8f, 0.8f);
 
-	glBindTexture(GL_TEXTURE_2D, 0);
+	glm::mat4 model;
+	GLint modelLoc = glGetUniformLocation(this->shaderProgram, "model");
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
 	glBindVertexArray(0);
+}
+
+void Demo::InitCamera()
+{
+	posCamX = 0.0f;
+	posCamY = 2.0f;
+	posCamZ = 8.0f;
+	viewCamX = 2.5f;
+	viewCamY = 1.0f;
+	viewCamZ = 0.0f;
+	upCamX = 0.0f;
+	upCamY = 1.0f;
+	upCamZ = 0.0f;
+	CAMERA_SPEED = 0.0001f;
+	fovy = 45.0f;
+	glfwSetInputMode(this->window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+}
+
+
+void Demo::MoveCamera(float speed)
+{
+	float x = viewCamX - posCamX;
+	float z = viewCamZ - posCamZ;
+	// forward positive cameraspeed and backward negative -cameraspeed.
+	posCamX = posCamX + x * speed;
+	posCamZ = posCamZ + z * speed;
+	viewCamX = viewCamX + x * speed;
+	viewCamZ = viewCamZ + z * speed;
+}
+
+void Demo::StrafeCamera(float speed)
+{
+	float x = viewCamX - posCamX;
+	float z = viewCamZ - posCamZ;
+	float orthoX = -z;
+	float orthoZ = x;
+
+	// left positive cameraspeed and right negative -cameraspeed.
+	posCamX = posCamX + orthoX * speed;
+	posCamZ = posCamZ + orthoZ * speed;
+	viewCamX = viewCamX + orthoX * speed;
+	viewCamZ = viewCamZ + orthoZ * speed;
+}
+
+void Demo::RotateCamera(float speed)
+{
+	float x = viewCamX - posCamX;
+	float z = viewCamZ - posCamZ;
+	viewCamZ = (float)(posCamZ + glm::sin(speed) * x + glm::cos(speed) * z);
+	viewCamX = (float)(posCamX + glm::cos(speed) * x - glm::sin(speed) * z);
 }
 
 int main(int argc, char** argv) {
 	RenderEngine &app = Demo();
-	app.Start("Texture Mapping: Draw textured cube and plane", 800, 600, false, false);
+	app.Start("Tugas Project Grafkom", 800, 600, false, false);
 }
